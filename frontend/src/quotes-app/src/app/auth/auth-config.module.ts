849bbaa59a74f1c12d4e1d48480de1849fa21af2
Vikash Chauhan <vikashchauhan51@gmail.com>
2023-02-04 21:00:20 +0530

    enable silent renew

--- frontend/src/quotes-app/src/app/auth/auth-config.module.ts
+++ frontend/src/quotes-app/src/app/auth/auth-config.module.ts
@@ -1,22 +1,29 @@
 import { NgModule } from '@angular/core';
 import { AuthModule, LogLevel  } from 'angular-auth-oidc-client';
-
+import { HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
     imports: [AuthModule.forRoot({
         config: {
             authority: 'https://localhost:5001',
-            redirectUrl: `https://localhost:4200/signin-callback`,
-            postLogoutRedirectUri: `https://localhost:4200/signout-callback`,
+            redirectUrl: `${window.location.origin}/signin-callback`,
+            postLogoutRedirectUri: `${window.location.origin}/signout-callback`,
             clientId: 'please-enter-clientId',
             scope: 'openid profile email offline_access', // 'openid profile ' + your scopes
             responseType: 'code',
-            silentRenew: false,
+            silentRenew: true,
+            silentRenewUrl: `${window.location.origin}/silent-renew.html`,
+            renewTimeBeforeTokenExpiresInSeconds: 10,
             useRefreshToken: true,
            logLevel: LogLevel.Debug,
 
         }
-      })],
+      }),
+      HttpClientModule,
+     CommonModule,
+     RouterModule],
     exports: [AuthModule],
 })
 export class AuthConfigModule {}
